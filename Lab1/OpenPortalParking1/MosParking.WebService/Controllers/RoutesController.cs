268567209a8a89b4c060f995f0d32449e58e0c22
ArtyomPariy <65407362+ArtyomPariy@users.noreply.github.com>
2020-05-15 17:26:24 +0300

    Add files via upload

---
+++ Lab1/OpenPortalParking1/MosParking.WebService/Controllers/RoutesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MosParking.DomainObjects;
+
+namespace MosParking.WebService.Controllers
+{
+    [ApiController]
+    [Parking("[controller]")]
+    public class ParkingsController : ControllerBase
+    {
+        private readonly ILogger<ParkingsController> _logger;
+
+        public ParkingsController(ILogger<ParkingsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Parking> GetParking()
+        {
+            return new List<Parking>() 
+            { 
+                new Parking() 
+                {
+                    Name = "ТПУ Кожуховская",
+                    PlaceNumber = "9909",
+                    AdmArea = "Юго-Восточный административный округ",
+                    District = "район Печатники",
+                    Address = "улица Южнопортовая, владение 3",
+                    Hours = "круглосуточно",
+                    CountSpaces = 62,
+                } 
+            };
+        }
+    }
+}
---
+++ Lab1/OpenPortalParking1/MosParking.WebService/DomainObjects/Ports/IParkingRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace MosParking.DomainObjects.Ports
+{
+    public interface IReadOnlyParkingRepository
+    {
+        Task<Parking> GetParking(long id);
+
+        Task<IEnumerable<Parking>> GetAllParking();
+
+        Task<IEnumerable<Parking>> QueryParking(ICriteria<Parking> criteria);
+
+    }
+
+    public interface IParkingRepository
+    {
+        Task AddParking(Parking parking);
+
+        Task RemoveParking(Parking parking);
+
+        Task UpdateParking(Parking parking);
+    }
+}
