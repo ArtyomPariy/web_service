8bb01adeefa0e72c91c1178e27562a15bf03b5b1
ArtyomPariy <65407362+ArtyomPariy@users.noreply.github.com>
2020-05-15 17:30:04 +0300

    Add files via upload

---
+++ Lab2/OpenPortalParking2/MosParking.WebService/ApplicationServices/GetParkingListUseCase/IGetParkingListUseCase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MosParking.ApplicationServices.Interfaces;
+
+namespace MosParking.ApplicationServices.GetRouteListUseCase
+{
+    public interface IGetParkingListUseCase : IUseCase<GetParkingListUseCaseRequest, GetParkingListUseCaseResponse>
+    {
+    }
+}
